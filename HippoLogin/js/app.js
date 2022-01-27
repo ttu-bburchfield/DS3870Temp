8c5c2eacc3eace36ef7f8801690b24d81227de68
Ben Burchfield <89394845+ttu-bburchfield@users.noreply.github.com>
2022-01-27 16:21:10 -0600

    Updates for web service and promise

--- HippoLogin/js/app.js
+++ HippoLogin/js/app.js
@@ -1,16 +1,42 @@
 $(document).on('click','#btnSignIn',function(){
-    console.log('btnSignIn Clicked');
-    let strUsername = '';
-    strUsername = $('#txtEmail').val();
-    console.log('Set value of strUsername = ' + strUsername);
-    localStorage.setItem('Username',strUsername);
-    console.log('Set localStorage Username');
-    console.log($('#txtPassword').val());
-
-
-    if($('#txtEmail').val() && $('#txtPassword').val()){
-        console.log('Username and Password exists');
-    } else {
-        console.log('Username or Password Blank');
+    var objNewSessionResponse;
+    let blnError = false;
+    let strErrorMessage = '';
+    if(!$('#txtEmail').val()){
+        blnError = true;
+        strErrorMessage += '<p>Email/Username is Blank</p>';
     }
+    if(!$('#txtPassword').val()){
+        blnError = true;
+        strErrorMessage += '<p>Password is Blank</p>';
+    }
+
+    if(blnError == true){
+        Swal.fire({
+            icon: 'error',
+            title: 'Missing Data',
+            html: strErrorMessage
+        })
+    } else {
+        var objNewSessionPromise = $.post('https://www.swollenhippo.com/DS3870/newSession.php', { strUsername:$('#txtEmail').val(), strPassword: $('#txtPassword').val() }, function(result){
+            objNewSessionResponse = JSON.parse(result);
+        })
+    }
+
+    $.when(objNewSessionPromise).done(function() {
+            if(objNewSessionResponse.Outcome == 'Login Failed'){
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Login Failed',
+                    html: '<h3>Please review your username and password</h3>'
+                }) 
+            } else {
+                sessionStorage.setItem('HippoSessionID',objNewSessionResponse.Outcome);
+                Swal.fire({
+                    icon: 'success',
+                    title: 'Login Complete',
+                    html: '<h3>Great Job!</h3>'
+                })
+            }
+    })
 })
